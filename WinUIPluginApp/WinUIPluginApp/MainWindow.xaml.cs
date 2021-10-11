93cb113486b96fd64ec31f4564e3210f9a6e665c
William Figtree <William.Figtree@resmed.com.au>
2021-10-11 14:33:50 +1100

    Add project files.

---
+++ WinUIPluginApp/WinUIPluginApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIPluginApp
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            pluginHost.Content = LoadPlugin("Plugin");
+        }
+
+        private object LoadPlugin(string name)
+        {
+            var root = Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            var path = Path.Combine(root, $"{name}\\{name}.dll");
+            var context = new PluginLoadContext(path);
+            var assembly = context.LoadFromAssemblyPath(path);
+            var rootControl = Activator.CreateInstance(assembly.GetType($"{name}.PluginRoot"));
+
+            return rootControl;
+        }
+    }
+}
